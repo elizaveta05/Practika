7a89947e29eb6d58dc7a11e0270fe13a54ccf9a1
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-23 18:40:33 +0700

    Разработка первого листа анкеты

--- Server/Controllers/AuthoController.cs
+++ Server/Controllers/AuthoController.cs
@@ -21,7 +21,6 @@ namespace Server.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] UserDataRegister user)
         {
-            // Проверка, существует ли уже пользователь с таким логином
             var existingUser = await _context.Userdataregisters
                 .FirstOrDefaultAsync(u => u.Login == user.Login);
 
@@ -30,17 +29,13 @@ namespace Server.Controllers
                 return BadRequest("Пользователь с таким логином уже существует.");
             }
 
-            // Хеширование пароля
             string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
-
-            // Получение максимального UserId и установка нового Id
             var maxUserId = await _context.Userdataregisters.MaxAsync(u => (int?)u.UdrId) ?? 0;
             var newUserId = maxUserId + 1;
 
-            // Создание нового пользователя, используя класс Userdataregister
             var newUser = new Userdataregister
             {
-                UdrId = newUserId, // Установка нового UserId
+                UdrId = newUserId,
                 Login = user.Login,
                 Password = hashedPassword
             };
@@ -48,7 +43,77 @@ namespace Server.Controllers
             _context.Userdataregisters.Add(newUser);
             await _context.SaveChangesAsync();
 
-            return Ok("Пользователь успешно зарегистрирован.");
+            return Ok(new { userId = newUserId });
         }
+
+        [HttpGet("cities")]
+        public async Task<IActionResult> GetCities()
+        {
+            var cities = await _context.Locations
+                .Select(c => new { c.LocationId, c.LocationName })
+                .ToListAsync();
+
+            return Ok(cities);
+        }
+
+        [HttpPost("newDataUser")]
+        public async Task<IActionResult> CreateNewDataUser([FromBody] Datauser newUser)
+        {
+            if (newUser == null)
+            {
+                return BadRequest("Некорректные данные.");
+            }
+
+            // Проверка обязательных данных
+            if (newUser.GenderId <= 0)
+            {
+                return BadRequest("Пол пользователя обязателен.");
+            }
+
+            if (newUser.LocationId <= 0)
+            {
+                return BadRequest("Местоположение пользователя обязательно.");
+            }
+
+            if (newUser.UdrId <= 0)
+            {
+                return BadRequest("ID пользователя обязателен.");
+            }
+
+            // Проверка наличия внешних ключей в базе данных
+            var genderExists = await _context.Genders.AnyAsync(g => g.GenderId == newUser.GenderId);
+            if (!genderExists)
+            {
+                return BadRequest("Некорректный идентификатор пола.");
+            }
+
+            var locationExists = await _context.Locations.AnyAsync(l => l.LocationId == newUser.LocationId);
+            if (!locationExists)
+            {
+                return BadRequest("Некорректный идентификатор местоположения.");
+            }
+
+            var udrExists = await _context.Userdataregisters.AnyAsync(u => u.UdrId == newUser.UdrId);
+            if (!udrExists)
+            {
+                return BadRequest("Некорректный идентификатор регистрации пользователя.");
+            }
+
+            // Получаем максимальный UserId и создаем новый
+            var maxUserId = await _context.Datausers.MaxAsync(u => (int?)u.UserId) ?? 0;
+            newUser.UserId = maxUserId + 1; // Увеличиваем на 1 для нового ID
+
+            var existingUser = await _context.Datausers.FirstOrDefaultAsync(u => u.UserId == newUser.UserId);
+            if (existingUser != null)
+            {
+                return BadRequest("Пользователь с таким идентификатором уже существует.");
+            }
+
+            _context.Datausers.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Пользователь успешно создан." });
+        }
+
     }
 }
---
+++ Server/Model/Model-users/DataUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makets.Model.Model_users;
+
+public partial class DataUser()
+{
+    public int UserId { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string? Patronymic { get; set; }
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public int GenderId { get; set; }
+
+    public int LocationId { get; set; }
+
+    public int UdrId { get; set; }
+}
