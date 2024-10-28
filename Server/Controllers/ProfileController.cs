0dcf323e787bd305992bc4eddb443165e505cef9
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-28 18:08:17 +0700

    Создание разметки для окон редактирования профиля и разработка первого окна.

--- Server/Controllers/ProfileController.cs
+++ Server/Controllers/ProfileController.cs
@@ -19,7 +19,7 @@ namespace Server.Controllers
             _context = context;
         }
 
-        // Вывод информации о пользователе
+        // Вывод информации о пользователе в профиль
         [HttpPost("getProfileUser")]
         public async Task<IActionResult> GetProfileUser([FromBody] int userId)
         {
@@ -53,6 +53,89 @@ namespace Server.Controllers
                 City = user.Location.LocationName,
                 Gender = user.Gender.GenderName
             });
+        }
+
+        // Вывод информации о пользователе из таблицы DataUser
+        [HttpPost("getDataUser")]
+        public async Task<IActionResult> GetDataUser([FromBody] int userId)
+        {
+            if (userId == 0)
+            {
+                return BadRequest(new { message = "Ошибка! Неверный ID пользователя." });
+            }
+
+            // Проверяем, существует ли пользователь
+            var userExists = await _context.Datausers.AnyAsync(u => u.UserId == userId);
+            if (!userExists)
+            {
+                return NotFound("Пользователь с таким id не существует");
+            }
+
+            var user = await _context.Datausers
+                                     .Include(u => u.Gender)
+                                     .Include(u => u.Location)
+                                     .FirstOrDefaultAsync(u => u.UserId == userId);
+
+            if (user == null)
+            {
+                return NotFound(new { message = "Данные о пользователе не найдены!" });
+            }
+
+            return Ok(new DataUser
+            {
+                LastName = user.LastName,
+                FirstName = user.FirstName,
+                Patronymic = user.Patronymic ?? null,
+                DateOfBirth = user.DateOfBirth,
+                GenderId = user.GenderId,
+                LocationId = user.LocationId,
+                UdrId = user.UdrId
+            });
+        }
+
+        //Обновление записи в таблице DataUser
+        [HttpPost("updateUserData")]
+        public async Task<IActionResult> UpdateUserData([FromBody] DataUser newUser)
+        {
+            // Проверка наличия внешних ключей
+            var genderExists = await _context.Genders.AnyAsync(g => g.GenderId == newUser.GenderId);
+            if (!genderExists)
+            {
+                return BadRequest("Некорректный идентификатор пола.");
+            }
+
+            var locationExists = await _context.Locations.AnyAsync(l => l.LocationId == newUser.LocationId);
+            if (!locationExists)
+            {
+                return BadRequest("Некорректный идентификатор местоположения.");
+            }
+
+            var udrExists = await _context.Userdataregisters.AnyAsync(u => u.UdrId == newUser.UdrId);
+            if (!udrExists)
+            {
+                return BadRequest("Некорректный идентификатор регистрации пользователя.");
+            }
+
+            // Поиск существующего пользователя
+            var existingUser = await _context.Datausers.FindAsync(newUser.UserId);
+            if (existingUser == null)
+            {
+                return NotFound("Пользователь не найден.");
+            }
+
+            // Обновление остальных полей пользователя
+            existingUser.LastName = newUser.LastName;
+            existingUser.FirstName = newUser.FirstName;
+            existingUser.Patronymic = newUser.Patronymic ?? null;
+            existingUser.DateOfBirth = newUser.DateOfBirth;
+            existingUser.GenderId = newUser.GenderId;
+            existingUser.LocationId = newUser.LocationId;
+            existingUser.UdrId = newUser.UdrId;
+
+            _context.Datausers.Update(existingUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { userId = existingUser.UserId });
         }
 
     }
