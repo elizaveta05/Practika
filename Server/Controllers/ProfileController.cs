83fb65eec8466f56a24475161c3bcb824541191e
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-28 12:06:34 +0700

    add

---
+++ Server/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using BD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Model.Model_users;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly PracticeDatingAppContext _context;
+
+        public ProfileController(PracticeDatingAppContext context)
+        {
+            _context = context;
+        }
+
+        // Вывод информации о пользователе
+        [HttpPost("getProfileUser")]
+        public async Task<IActionResult> getProfileUser([FromBody] int userId)
+        {
+            //Проверяем id
+            if (userId.Equals(null))
+            {
+                return Unauthorized(new { message = "Ошибка!" });
+            }
+
+            // Ищем пользователя по логину
+            var user = await _context.Datausers.FirstOrDefaultAsync(u => u.UserId == userId);
+
+            //Проверяем находится ли пользователь
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Данные о пользователе не найдены!" });
+            }
+
+
+            return Ok();
+        }
+    }
+}
---
+++ Server/Model/Model-users/ProfileUser.cs
@@ -0,0 +1,19 @@
+﻿namespace Server.Model.Model_users
+{
+    public class ProfileUser
+    {
+        public int UserId { get; set; }
+
+        public string LastName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string? Patronymic { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public int GenderId { get; set; }
+
+        public int LocationId { get; set; }
+    }
+}
