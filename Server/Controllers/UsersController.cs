300dbcf5679c0dbb184b8a836484030561242f40
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-31 19:33:46 +0700

    Смена цвета чата

--- Server/Controllers/UsersController.cs
+++ Server/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using BD.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Server.Controllers
 {
@@ -15,7 +16,17 @@ namespace Server.Controllers
             _context = context;
         }
 
+        // Метод для получения логина пользователя по его ID
+        [HttpGet("getUsername/{userId}")]
+        public async Task<IActionResult> GetUsername(int userId)
+        {
+            var user = await _context.Datausers.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound("Пользователь не найден.");
+            }
 
-
+            return Ok(user.FirstName);
+        }
     }
 }
